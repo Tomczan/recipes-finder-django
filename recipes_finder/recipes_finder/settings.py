54c611adc3d43e33989dff9e7ddee4872aea6806
Tomczan <t.czuba95@gmail.com>
2022-05-04 18:28:05 +0200

    changed database from SQLite to postgreSQL
    add: requirements.txt file which contain all required packages to run project

--- recipes_finder/recipes_finder/settings.py
+++ recipes_finder/recipes_finder/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'recipes_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipe_finder_db',
+        'USER': config('DB_LOCAL_USER'),
+        'PASSWORD': config('DB_LOCAL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
