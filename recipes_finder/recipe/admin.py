d48a10baa639bc1ee7bc607a59a8a9b6961baa98
Tomczan <t.czuba95@gmail.com>
2022-05-21 17:29:12 +0200

    add: models needed to create Recipe
    registered these models in the admin panel

--- recipes_finder/recipe/admin.py
+++ recipes_finder/recipe/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Recipe, Integrent, IntegrentType, RecipeIntegrents
 
 # Register your models here.
+
+
+class RecipeIntegrentsInLine(admin.TabularInline):
+    model = RecipeIntegrents
+    extra = 1
+
+
+@admin.register(IntegrentType)
+class IntegrentTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Integrent)
+class IntegrentAdmin(admin.ModelAdmin):
+    inlines = (RecipeIntegrentsInLine,)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIntegrentsInLine,)
--- recipes_finder/recipe/models.py
+++ recipes_finder/recipe/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+# https://github.com/dgrant/django_recipes/blob/ff68768fd3b1282f02bae3a041e624b8d5338f16/recipes/models.py#L319
+
+UNIT_CHOICES = [
+    ('g', 'gram'),
+    ('kg', 'kilogram'),
+    ('ml', 'mililitr'),
+    ('l', 'litr'),
+]
+
+
+class IntegrentType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Integrent(models.Model):
+    name = models.CharField(max_length=200)
+    prefered_unit = models.CharField(
+        max_length=2, choices=UNIT_CHOICES, blank=False)
+    type = models.ForeignKey(IntegrentType, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    # image = models.ImageField(
+    #     upload_to='recipes/%Y/%m/%d', height_field=None, width_field=None, max_length=100)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    is_visible = models.BooleanField(default=False)
+    integrents = models.ManyToManyField(Integrent, through='RecipeIntegrents')
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class RecipeIntegrents(models.Model):
+    amount = models.IntegerField(default=0)
+    unit = models.CharField(max_length=2, choices=UNIT_CHOICES, blank=False)
+    integrent = models.ForeignKey(Integrent, on_delete=models.PROTECT)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
