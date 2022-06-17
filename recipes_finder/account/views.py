e4a2371d9852813da2675a1f6213ad30081bf371
Tomczan <t.czuba95@gmail.com>
2022-06-17 22:37:08 +0200

    add: CBV user_login

--- recipes_finder/account/views.py
+++ recipes_finder/account/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.views import View
 
 # Create your views here.
+
+
+class user_login(View):
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=form['username'],
+                                password=form['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnianie pomyślne')
+                else:
+                    return HttpResponse('Konto jest zablokowane')
+        else:
+            return HttpResponse('Nieprawidłowe dane uwierzytelniajace')
+
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'account/login.html', {'form': form})
