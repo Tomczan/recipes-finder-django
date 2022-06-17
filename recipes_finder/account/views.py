85d908e348cef031291b6844e6f669dd697cfb49
Tomczan <t.czuba95@gmail.com>
2022-06-17 23:51:28 +0200

    fixed issues in user_login view
    add: urls and template for user login

--- recipes_finder/account/views.py
+++ recipes_finder/account/views.py
@@ -12,8 +12,8 @@ class user_login(View):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=form['username'],
-                                password=form['password'])
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
