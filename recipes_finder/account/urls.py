236e7bde538d37e76388a102f988286fb11ab837
Tomczan <t.czuba95@gmail.com>
2022-06-27 17:49:20 +0200

    add: built-in django views for Login and Logout

--- recipes_finder/account/urls.py
+++ recipes_finder/account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import user_login
+from django.contrib.auth import views as auth_views
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', user_login.as_view(), name='login')
+    # path('login/', user_login.as_view(), name='login')
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
