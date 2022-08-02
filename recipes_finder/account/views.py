ad3f2e2b870214d93ca1669b7f8a4525aeab1c40
Tomczan <t.czuba95@gmail.com>
2022-08-02 22:03:42 +0200

    add: user registrations

--- recipes_finder/account/views.py
+++ recipes_finder/account/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.views import View
 
 # Create your views here.
 
 
-class user_login(View):
+# class UserLogin(View):
+#     def post(self, request):
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authentication completed successfully')
+#                 else:
+#                     return HttpResponse('Account is locked')
+#         else:
+#             return HttpResponse('Invalid credentials')
+
+#     def get(self, request):
+#         form = LoginForm()
+#         return render(request, 'account/login.html', {'form': form})
+
+class RegisterView(View):
     def post(self, request):
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Uwierzytelnianie pomyślne')
-                else:
-                    return HttpResponse('Konto jest zablokowane')
-        else:
-            return HttpResponse('Nieprawidłowe dane uwierzytelniajace')
+        registration_form = UserRegistrationForm(request.POST)
+        if registration_form.is_valid():
+            new_user = registration_form.save(commit=False)
+            new_user.set_password(registration_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'registration/register_done.html',
+                          {'new_user': new_user})
 
     def get(self, request):
-        form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+        registration_form = UserRegistrationForm()
+        return render(request,
+                      'registration/register.html',
+                      {'registration_form': registration_form})
