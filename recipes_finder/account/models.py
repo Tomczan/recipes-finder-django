acc59249bd2c812838c555ab329b72c01df4fe32
Tomczan <t.czuba95@gmail.com>
2022-08-05 17:48:16 +0200

    Extension of User model using Profile model with one-to-one relationship
    Installed Pillow and configured settings to handle media files for the development server

--- recipes_finder/account/models.py
+++ recipes_finder/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    nickname = models.TextField(max_length=150)
+    photo = models.ImageField(blank=True, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
