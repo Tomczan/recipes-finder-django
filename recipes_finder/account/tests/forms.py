26cf3eeea0d4225a28faed602bb6608087ba0ed2
Tomczan <t.czuba95@gmail.com>
2022-08-11 18:44:57 +0200

    UserRegistrationForm tests

---
+++ recipes_finder/account/tests/forms.py
@@ -0,0 +1,28 @@
+from xml.dom import ValidationErr
+from django.test import TestCase
+from account.forms import UserRegistrationForm
+from django import forms
+
+
+class UserRegistrationFormTestCase(TestCase):
+
+    def test_valid_clean_password2(self):
+        data = {
+            'username': 'test_a',
+            'password': 'test_password',
+            'password2': 'test_password'
+        }
+        form = UserRegistrationForm(data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.clean_password2(), 'test_password')
+
+    def test_invalid_clean_password2(self):
+        data = {
+            'username': 'test_a',
+            'password': 'test_password',
+            'password2': 'wrong_password'
+        }
+        form = UserRegistrationForm(data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(str(form['password2'].errors.as_data(
+        )), "[ValidationError(['Passwords are not the same.'])]")
