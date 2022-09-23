a39c292c76682aae6be27061e8e900d913d82df0
Tomczan <t.czuba95@gmail.com>
2022-09-23 18:57:56 +0200

    add: tests POST method for RecipeCreateView

--- recipes_finder/recipe/tests/test_views.py
+++ recipes_finder/recipe/tests/test_views.py
@@ -1,6 +1,8 @@
-from django.test import TestCase, Client, RequestFactory
+from unicodedata import name
+from venv import create
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
-from recipe.models import Recipe
+from recipe.models import Recipe, IngredientType, Ingredient
 from django.contrib.auth.models import User
 from recipe.views import UserRecipesListView
 
@@ -78,6 +80,12 @@ class RecipeCreateViewTestCase(TestCase):
         self.user.set_password('12345')
         self.user.save()
         self.url = reverse('recipe:recipe_create')
+        self.ingredient_type = IngredientType.objects.create(
+            name='type_for_tests')
+        Ingredient.objects.create(
+            id=1, name='Salami', prefered_unit='g', type=self.ingredient_type)
+        Ingredient.objects.create(
+            id=2, name='Tomato', prefered_unit='g', type=self.ingredient_type)
 
     def test_GET_not_logged_in(self):
         response = self.client.get(self.url)
@@ -89,3 +97,65 @@ class RecipeCreateViewTestCase(TestCase):
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_POST_recipe_without_ingredients(self):
+        self.client.login(username='user1', password='12345')
+        response = self.client.post(self.url, {
+            'name': ['Pizza'],
+            'description': ['Pizza with salami'],
+            'instructions': ['1. Order\r\n2.Eat'],
+            'recipe_to_ingredient-TOTAL_FORMS': ['0'],
+            'recipe_to_ingredient-INITIAL_FORMS': ['0'],
+            'recipe_to_ingredient-MIN_NUM_FORMS': ['0'],
+            'recipe_to_ingredient-MAX_NUM_FORMS': ['1000'],
+            'recipe_to_ingredient-__prefix__-quantity': ['0'],
+            'recipe_to_ingredient-__prefix__-unit': [''],
+            'recipe_to_ingredient-__prefix__-ingredient': [''],
+            'recipe_to_ingredient-__prefix__-recipe': [''],
+            'recipe_to_ingredient-__prefix__-id': ['']
+        })
+        created_recipe = Recipe.objects.get(name='Pizza')
+        ingredient_count = len(created_recipe.ingredients.all())
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(created_recipe.name, 'Pizza')
+        self.assertEqual(created_recipe.description, 'Pizza with salami')
+        self.assertEqual(created_recipe.instructions, '1. Order\r\n2.Eat')
+        self.assertEqual(ingredient_count, 0)
+
+    def test_POST_recipe_with_2_ingredients(self):
+        self.client.login(username='user1', password='12345')
+        response = self.client.post(self.url, {
+            'name': ['Pizza'],
+            'description': ['Pizza with salami and tomatoes.'],
+            'instructions': ['1. Example instructions...'],
+            'recipe_to_ingredient-TOTAL_FORMS': ['2'],
+            'recipe_to_ingredient-INITIAL_FORMS': ['0'],
+            'recipe_to_ingredient-MIN_NUM_FORMS': ['0'],
+            'recipe_to_ingredient-MAX_NUM_FORMS': ['1000'],
+            'recipe_to_ingredient-0-quantity': ['300'],
+            'recipe_to_ingredient-0-unit': ['g'],
+            'recipe_to_ingredient-0-ingredient': ['1'],
+            'recipe_to_ingredient-0-recipe': [''],
+            'recipe_to_ingredient-0-id': [''],
+            'recipe_to_ingredient-1-quantity': ['150'],
+            'recipe_to_ingredient-1-unit': ['g'],
+            'recipe_to_ingredient-1-ingredient': ['2'],
+            'recipe_to_ingredient-1-recipe': [''],
+            'recipe_to_ingredient-1-id': [''],
+            'recipe_to_ingredient-__prefix__-quantity': ['0'],
+            'recipe_to_ingredient-__prefix__-unit': [''],
+            'recipe_to_ingredient-__prefix__-ingredient': [''],
+            'recipe_to_ingredient-__prefix__-recipe': [''],
+            'recipe_to_ingredient-__prefix__-id': ['']
+        })
+        created_recipe = Recipe.objects.get(name='Pizza')
+        ingredient_count = len(created_recipe.ingredients.all())
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(created_recipe.name, 'Pizza')
+        self.assertEqual(created_recipe.description,
+                         'Pizza with salami and tomatoes.')
+        self.assertEqual(created_recipe.instructions,
+                         '1. Example instructions...')
+        self.assertEqual(ingredient_count, 2)
