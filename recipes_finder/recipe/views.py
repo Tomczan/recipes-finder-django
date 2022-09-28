5da2da4cacddc13497a316207b5bdf32f911b709
Tomczan <t.czuba95@gmail.com>
2022-09-28 19:46:32 +0200

    view order changed to keep similar views next to each other

--- recipes_finder/recipe/views.py
+++ recipes_finder/recipe/views.py
@@ -3,9 +3,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.forms import inlineformset_factory
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .forms import RecipeCreateForm, RecipeIngredientsForm
 from .models import Recipe, RecipeIngredients
@@ -24,9 +23,50 @@ class RecipeList(ListView):
     paginate_by = 10
 
 
+class UserRecipesListView(ListView):
+    model = Recipe
+    template_name = 'recipe/my_recipes.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        return Recipe.objects.filter(author=self.request.user)
+
+
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe/detail.html'
+
+
+@login_required
+def recipe_create_view(request):
+    IngredientInlineFormSet = inlineformset_factory(Recipe,
+                                                    RecipeIngredients,
+                                                    form=RecipeIngredientsForm,
+                                                    extra=0,
+                                                    can_delete=False)
+    recipe_form = RecipeCreateForm(request.POST or None)
+    formset = IngredientInlineFormSet(request.POST or None)
+    if recipe_form.is_valid() and formset.is_valid():
+        recipe = recipe_form.save(commit=False)
+        recipe.author = request.user
+        recipe.save()
+        formset = formset.cleaned_data
+        for form in formset:
+            if form:
+                ingredient = RecipeIngredients(quantity=form['quantity'],
+                                               unit=form['unit'],
+                                               ingredient=form['ingredient'],
+                                               recipe=recipe)
+                ingredient.save()
+        messages.success(
+            request, f'The recipe "{recipe.name}" has been added.')
+        return redirect('recipe:recipe_create')
+    template_name = 'recipe/create.html'
+    context = {
+        "form": recipe_form,
+        "formset": formset
+    }
+    return render(request, template_name, context)
 
 
 @login_required
@@ -65,45 +105,3 @@ def recipe_update_view(request, slug, id):
         "formset": formset
     }
     return render(request, template_name, context)
-
-
-@login_required
-def recipe_create_view(request):
-    IngredientInlineFormSet = inlineformset_factory(Recipe,
-                                                    RecipeIngredients,
-                                                    form=RecipeIngredientsForm,
-                                                    extra=0,
-                                                    can_delete=False)
-    recipe_form = RecipeCreateForm(request.POST or None)
-    formset = IngredientInlineFormSet(request.POST or None)
-    if recipe_form.is_valid() and formset.is_valid():
-        recipe = recipe_form.save(commit=False)
-        recipe.author = request.user
-        recipe.save()
-        formset = formset.cleaned_data
-        for form in formset:
-            if form:
-                ingredient = RecipeIngredients(quantity=form['quantity'],
-                                               unit=form['unit'],
-                                               ingredient=form['ingredient'],
-                                               recipe=recipe)
-                ingredient.save()
-        messages.success(
-            request, f'The recipe "{recipe.name}" has been added.')
-        return redirect('recipe:recipe_create')
-    template_name = 'recipe/create.html'
-    context = {
-        "form": recipe_form,
-        "formset": formset
-    }
-    return render(request, template_name, context)
-
-
-class UserRecipesListView(ListView):
-    model = Recipe
-    template_name: str = 'recipe/my_recipes.html'
-    paginate_by: int = 20
-
-    def get_queryset(self):
-        return Recipe.objects.filter(author=self.request.user)
-        # return Recipe.objects.all()
